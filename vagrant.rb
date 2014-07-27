1705271ea0affcda5f7799bde0b2397cb3926746
Ryan Brewster <brewster1134+tower@gmail.com>
2014-07-26 23:03:21 -0700

    vagrant plugins

--- vagrant.rb
+++ vagrant.rb
@@ -6,20 +6,37 @@ class Yuyi::Vagrant < Yuyi::Roll
     :version => {
       :description => 'Set an older version of vagrant to install (1.4.3, 1.2.7).  Leave blank for the latest version.',
       :example => '1.4.3'
+    },
+    :plugins => {
+      :description => 'Array of vagrant plugins to install',
+      :example => ['vagrant-example-plugin'],
+      :default => []
     }
   })
 
   install do
     run "brew cask install vagrant#{version}"
+
+    options[:plugins].each do |plugin|
+      run "vagrant plugin install #{plugin}"
+    end
   end
 
   uninstall do
     run "brew cask uninstall vagrant#{version}"
+
+    options[:plugins].each do |plugin|
+      run "vagrant plugin uninstall #{plugin}"
+    end
   end
 
   upgrade do
     run "brew cask install vagrant#{version} --force"
     run 'brew cask cleanup'
+
+    options[:plugins].each do |plugin|
+      run "vagrant plugin update #{plugin}"
+    end
   end
 
   installed? do
@@ -30,6 +47,3 @@ class Yuyi::Vagrant < Yuyi::Roll
     (options[:version] || '').scan(/[0-9]/).join
   end
 end
-
-
-# TODO: plugins
